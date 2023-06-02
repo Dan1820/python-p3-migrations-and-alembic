c9d630fd50a20967dd7de54951c2a09db6b30388
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-06-02 20:12:08 +0300

    done

---
+++ lib/migrations/versions/0e3d297760b3_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 0e3d297760b3
+Revises: 
+Create Date: 2023-06-02 19:48:10.214752
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0e3d297760b3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
